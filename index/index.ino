1f0749567201ae40c5f1f0dae9ac975684a0b420
NirBenita <benitanir@gmail.com>
2015-10-06 01:57:03 +0300

    Prettify code

--- index/index.ino
+++ index/index.ino
@@ -1,17 +1,16 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 35
-#define STRIP    6
+#define NUM_LEDS 35 // The number of LEDs in our strip
+#define STRIP    6 // The pin to which the strip is connected
+const int sensorPin   = A0; // The pin to which the sensor is connected
 
-const int sensorPin   = A0;
-int pinToLight   = 0,
-    airPressure,
-    pressureToRGB,
-    interval     = 300,
-    high            = 0,
-    low             = 600;
-long lastMillis  = 0;
-boolean isOn     = false;
+int ledToLight   = 0, // The specific LED from within the array
+    airPressure, // The current state of the pressure in our system
+    pressureToRGB, // A representation of airPressure between 1-255
+    interval     = 300, // The speed at which we animate
+    high            = 0, // Maximum air pressure
+    low             = 600; // Static air pressure
+long lastMillis  = 0; // The last time we called blinkAndStep()
 
 // Constructor functions
 CRGB leds[NUM_LEDS];
@@ -19,23 +18,19 @@ CRGB leds[NUM_LEDS];
 void setup() {
   Serial.begin(115200);
   pinMode(sensorPin,INPUT);
-  // Setup the LED strip
-  FastLED.addLeds<NEOPIXEL, STRIP>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, STRIP>(leds, NUM_LEDS); // Setup the LED strip
 }
  
 void loop() {
-  airPressure = analogRead(sensorPin);
-  Serial.print("Air Pressure:");
-  Serial.println(airPressure);
+  airPressure = analogRead(sensorPin); // First read
   autoTune();
-  Serial.print("After auto Tune:");
-  Serial.println(airPressure);
   if (millis() - lastMillis > interval/airPressure){
     blinkAndStep();
     lastMillis = millis();
   }
 }
 
+// Determine the spectrum of air pressure
 void autoTune() {
   if (airPressure < low) {
     low = airPressure;
@@ -48,15 +43,14 @@ void autoTune() {
   airPressure = map(airPressure, low, high, 1, 200);
   airPressure = constrain(airPressure, 1, 200);
 }
-
+// Blink an LED, and set the position of the next LED to light
 void blinkAndStep() {
-   if (pinToLight > NUM_LEDS){
-    pinToLight=0;
+   if (ledToLight > NUM_LEDS){
+    ledToLight=0;
   };
-  leds[pinToLight].setRGB(pressureToRGB, 68, 255/pressureToRGB);;
+  leds[ledToLight].setRGB(pressureToRGB, 68, 255/pressureToRGB);;
   FastLED.show();
-  leds[pinToLight] = CRGB::Black;
+  leds[ledToLight] = CRGB::Black;
   FastLED.show();
-  pinToLight++;
+  ledToLight++;
 }
-
