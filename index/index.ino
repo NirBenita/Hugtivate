e741fcbeef3992938aecfc120430f78008c926ea
NirBenita <benitanir@gmail.com>
2015-10-31 12:41:49 +0200

    interval functionality

--- index/index.ino
+++ index/index.ino
@@ -4,13 +4,20 @@
 #define STRIP    6 // The pin to which the strip is connected
 const int sensorPin   = A0; // The pin to which the sensor is connected
 
-int ledToLight   = 0, // The specific LED from within the array
-    airPressure, // The current state of the pressure in our system
+int ledToLight      = 0, // The specific LED from within the array
+    airPressure     = 0, // The current state of the pressure in our system
     pressureToRGB, // A representation of airPressure between 1-255
-    interval     = 500, // The speed at which we animate
-    high            = 0,    // Initiate spectrum ends
-    low             = 600; // 
-long lastMillis  = 0; // The last time we called blinkAndStep()
+    interval        = 500, // The speed at which we animate
+    maxMessured     = 0,    // Initiate spectrum ends
+    minMessured     = 600, // 
+    maxSpectrum     = 255,
+    minSpectrum     = 1,
+    prevAirPressure = 0,
+    isHugging       = false,
+    superHugDuration= 5000,
+    hugStartTime;
+
+long lastMillis     = 0; // The last time we called blinkAndStep()
 
 // Constructor functions
 CRGB leds[NUM_LEDS];
@@ -22,30 +29,36 @@ void setup() {
 }
  
 void loop() {
+  prevAirPressure = airPressure;
   airPressure = analogRead(sensorPin); // First read
   autoTune();
-  if (millis() - lastMillis > interval/airPressure){
-    blinkAndStep();
-    lastMillis = millis();
+  if (prevAirPressure < maxSpectrum/2 && airPressure > maxSpectrum/2) {
+    hugStartTime = millis();
+    isHugging = true;
+  } else if (airPressure < maxSpectrum/2) {
+    isHugging = false;
+  }
+  if (millis()-hugStartTime > superHugDuration && isHugging) {
+    lightEmUp();
   }
 }
 
 // Determine the spectrum of air pressure
 void autoTune() {
-  if (airPressure < low) {
-    low = airPressure;
+  if (airPressure < minMessured) {
+    minMessured = airPressure;
   }
-  if (airPressure > high) {
-    high = airPressure;
+  if (airPressure > maxMessured) {
+    maxMessured = airPressure;
   }
-  airPressure = map(airPressure, low, high, 1, 255);
-  airPressure = constrain(airPressure, 1, 255);
+  airPressure = map(airPressure, minMessured, maxMessured, minSpectrum, maxSpectrum);
+  airPressure = constrain(airPressure, minSpectrum, maxSpectrum);
 }
 // Blink an LED, and set the position of the next LED to light
 void blinkAndStep() {
    if (ledToLight > NUM_LEDS){
     ledToLight=0;
-  };
+    };
   leds[ledToLight].setRGB(airPressure, 68, 255/airPressure);;
   FastLED.show();
   leds[ledToLight] = CRGB::Black;
@@ -53,3 +66,19 @@ void blinkAndStep() {
   ledToLight++;
 }
 
+/*
+  airPressure was low, now high?
+    save start time
+    stillOn = true
+    pressure high?
+      millis()-startTime > X?
+        callback
+
+*/
+
+void lightEmUp() {
+  leds[0].setRGB(20,234,13);
+  FastLED.show();
+}
+
+        
